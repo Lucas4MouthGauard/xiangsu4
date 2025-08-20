842de6ce324713a4dbd888cf9c0a278959d7dcd9
Lucas4MouthGauard <biaoli888@outlook.com>
2025-08-20 23:21:00 +0800

    更新底部buy按钮跳转链接到PumpAlien代币页面

--- src/components/StoryReveal.tsx
+++ src/components/StoryReveal.tsx
@@ -235,7 +235,11 @@ const StoryReveal: React.FC<StoryRevealProps> = ({ currentChapter, onNextChapter
                   </p>
                   
                   {/* 行动按钮 */}
-                  <button id="buy-pumpalien-btn" className="rescue-cta-btn">
+                  <button 
+                    id="buy-pumpalien-btn" 
+                    className="rescue-cta-btn"
+                    onClick={() => window.open('https://pump.fun/coin/GRX469HNsy97moku9H1QHQGKmB3hSq3c6qETQ1oupump', '_blank')}
+                  >
                     <span className="btn-icon">💰</span>
                     <span className="btn-text">BUY $PumpAlien</span>
                   </button>
